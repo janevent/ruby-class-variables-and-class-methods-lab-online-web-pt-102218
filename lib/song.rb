6c6ed3ba0cf705679f8383ab762da3cf13323204
Jane Venter <janeanneventer@gmail.com>
2018-11-14 18:08:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,21 @@
 class Song 
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@genre_array = []
+  @@genres = []
   @@artists = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist 
     @genre = genre 
     @@count +=1
-    @@genre_array << @genre
+    @@genres << @genre
     @@artists << @artist
   end
   def count 
     @@count
   end
   def genres
-    @@genre_array.uniq!
+    @@genres.uniq!
   end
   def artists
     @@artists.uniq!
